f3e7a003aded842ffda6960d43729969f368baa3
Fritz Geib <fritz@standland.org>
2021-05-09 21:37:33 -0400

    add bash specific update file

---
+++ bashUpdate.sh
@@ -0,0 +1,18 @@
+updateType=""
+#sudo printf "Would you like to preform a full update? [y/n] "
+#read updateType
+#if [ $updateType == "y" ] || [ $updateType == "Y" ]
+    #then
+    #echo yeet
+#fi
+printf "\nRunning Update...\n\n"
+sleep 0.5
+sudo apt update
+printf "\nRunning Upgrade...\n\n"
+sleep 0.5
+sudo apt upgrade
+printf "\nRunning Auto Remove...\n\n"
+sleep 0.5
+sudo apt autoremove
+printf "\nRunning Snap Refresh...\n\n"
+sudo snap refresh
