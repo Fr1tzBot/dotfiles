b4b218859dc53a88115a41252610cac1f57b7803
Fritz Geib <fritz@standland.org>
2025-03-18 02:10:35 -0400

    start with tab completions for copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,7 +9,7 @@ return {
       auto_trigger = true,
       hide_during_completion = vim.g.ai_cmp,
       keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
+        accept = "<Tab>", -- handled by nvim-cmp / blink.cmp
         next = "<M-]>",
         prev = "<M-[>",
       },
