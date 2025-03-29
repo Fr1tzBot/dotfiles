d133985072d53f8872e4ec57b5750a9a81078167
Fritz Geib <fritz@standland.org>
2025-03-29 02:32:12 -0400

    make cmp actually work

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,4 +1,31 @@
 return {
-    "hrsh7th/nvim-cmp"
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
+    config = function()
+        local cmp = require("cmp")
+
+        cmp.setup({
+            mapping = {
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            },
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "buffer" },
+                { name = "path" },
+            }),
+            snippet = {
+                expand = function(args)
+                    vim.fn["vsnip#anonymous"](args.body)
+                end,
+            },
+        })
+    end,
 }
 
