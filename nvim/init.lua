3d4f3d3189f895679f7267b978bd6d99c70c5629
Fritz Geib <fritz@standland.org>
2025-03-30 04:06:10 -0400

    some magical vim 0.11 change and add lua server

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,11 +9,12 @@ require("nvim-treesitter.configs").setup({
 
 local ensure_installed = {
     "bash-language-server",
-    "java-language-server",
     "deno",
+    "java-language-server",
+    "lua-language-server",
     "matlab-language-server",
-    "pyright",
     "pylint",
+    "pyright",
     "rust-analyzer",
     "shellcheck",
 }
@@ -44,6 +45,15 @@ vim.diagnostic.config {
     underline = true,
     update_in_insert = false,
 }
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
+})
 
 vim.opt.termguicolors = true
 vim.opt.autoindent = true
