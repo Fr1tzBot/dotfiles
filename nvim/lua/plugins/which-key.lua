76bbf863e0fb6aa4dffc16159e2ffdb8645398a2
Fritz Geib <fritz@standland.org>
2025-03-31 16:01:25 -0400

    add some dependencies to which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,19 +1,24 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+    "folke/which-key.nvim",
+
+    event = "VeryLazy",
+    dependencies = {
+        { 'echasnovski/mini.nvim', version = '*' },
+        "nvim-tree/nvim-web-devicons"
     },
-  },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    keys = {
+        {
+        "<leader>?",
+        function()
+            require("which-key").show({ global = false })
+        end,
+    desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
 }
 
