043b01cde8bbab36fdf6a778ae35ac5dff5a6e2a
Fritz Geib <fritz@standland.org>
2025-03-31 15:55:45 -0400

    turn on lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,25 @@ require('telescope').setup{
     }
 }
 
+require("lualine").setup {
+    options = {
+        theme = "gruvbox",
+        icons_enabled = true,
+    },
+    sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff", "diagnostics"},
+        lualine_c = {"filename"},
+        lualine_x = {"encoding", "fileformat", "filetype"},
+        lualine_y = {"progress"},
+        lualine_z = {"location"}
+    }
+}
+-- disable default statusline
+vim.opt.laststatus = 3
+vim.opt.showtabline = 0
+
+
 vim.diagnostic.config {
     virtual_text = true,
     signs = true,
